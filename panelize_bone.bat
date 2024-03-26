093a2f25dba089eac7e5f4073ba733ea3a69e10c
ocanath <ocanath@gmail.com>
2024-03-26 16:28:08 -0700

    panelization options

--- panelize_bone.bat
+++ panelize_bone.bat
@@ -2,8 +2,8 @@
 
 kikit panelize ^
     --layout "grid; rows: 2; cols: 2; space: 1mm" ^
-    --tabs "fixed; width: 2mm; vcount: 0" ^
+    --tabs "fixed; width: 3mm; vcount: 0" ^
     --cuts "mousebites; drill: 0.3mm; spacing: 0.5mm; offset: -0.25mm; prolong: 0.5mm" ^
-    --framing "frame; width: 5mm; space: 3mm; cuts: both" ^
+    --framing "frame; width: 5mm; space: 1mm; cuts: none" ^
     --post "millradius: 0.2mm" ^
     dogbone.kicad_pcb dogbone-panel.kicad_pcb
